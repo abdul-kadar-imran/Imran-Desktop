0ea2f77d3c67fc7619903f8a921d92f58ac20c1b
A Imran <abdulkadarimran@gmail.com>
2026-07-10 23:20:00 +0530

    first commit

--- src/assets/images/client-projects.ts
+++ src/assets/images/client-projects.ts
@@ -1,9 +1,10 @@
 import crescentConnectIcon from './Bsa.png';
 import adityaSkillGateIcon from './Aditya skill gate.png';
-import echoesIcon from './Echoess.png';
+import echoesIcon from './Echoes.png';
 import digirfuIcon from './Digirfu.png';
 export const clientProjectIcons = {
   'crescent-connect': crescentConnectIcon,
   'aditya-skill-gate': adityaSkillGateIcon,
   'echoes': echoesIcon,
-  'digirfu': digirfuIcon,};
+  'digirfu': digirfuIcon,
+};
