8efe93165e4b6f33cf8582d724e5a5fa2ef307ab
A Imran <abdulkadarimran@gmail.com>
2026-07-11 21:45:47 +0530

    Fix Digirfu image import

--- src/assets/images/client-projects.ts
+++ src/assets/images/client-projects.ts
@@ -1,7 +1,7 @@
 import crescentConnectIcon from './Bsa.png';
 import adityaSkillGateIcon from './Aditya skill gate.png';
 import echoesIcon from './Echoes.png';
-import digirfuIcon from './Digirfu.png';
+import digirfuIcon from './digirfu.png';
 export const clientProjectIcons = {
   'crescent-connect': crescentConnectIcon,
   'aditya-skill-gate': adityaSkillGateIcon,
